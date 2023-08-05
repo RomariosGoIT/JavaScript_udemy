9f8ccf1e2233c8f97e58cc5a0a0ac63dec7d6201
Roma <romantgt@gmail.com>
2023-08-05 16:22:23 +0300

    logic operator lesson

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
 
-const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {  },
-  actors: {},
-  genres: [],
-  privat: false
-};
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {  },
+//   actors: {},
+//   genres: [],
+//   privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+const num = 120;
+
+switch(num) {
+  case 49: 
+    console.log('Error');
+    break;
+  case 100:
+    console.log('Error!');
+    break;
+  case 50:
+    console.log('Ok!');
+    break;
+  default:
+    console.log('Not this time!');
+    break;
+}
